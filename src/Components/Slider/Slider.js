9e39121fc34a52fa46df0953ee2843140227a889
ddidee <olawaleadeola20@gmail.com>
2022-12-19 18:09:22 +0100

    auto slide effect

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styl from '../../Assets/style1.png'
 import styl1 from '../../Assets/style4.png'
 import styl2 from '../../Assets/style3.png'
@@ -7,10 +7,26 @@ import "./Slider.css"
 const Slider = () => {
   const[currentState, setCurrentState]= useState(0)
 
+// with useEffect, we move through each slides automatically
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentState===2) {
+        setCurrentState(0)
+      }
+      else{
+        setCurrentState(currentState + 1)
+
+      }
+    }, 3000)
+    return () => clearTimeout(timer)
+    
+  }, [currentState])
+
   const data =[
     {url: styl, text:'LOOKING FOR SOMETHING ELEGANT?'},
     {url: styl1, text:'A FEEL OF LUXURY...'},
-    {url: styl2, text: 'SHOP STYLERAPY'}
+    {url: styl2, text: 'WELCOME TO SYLERAPY...'}
 
   ]
 
@@ -21,7 +37,9 @@ const Slider = () => {
     height: '100%'
   }
 
-  const goToNext = ()=>{
+  // A function to move to the next slide
+
+  const goToNext = currentState => {
     setCurrentState(currentState)
   }
   
@@ -32,7 +50,10 @@ const Slider = () => {
         <div className='Overlay'></div>
         <h2>{data[currentState].text}</h2>
         <button>SHOP NOW</button>
-        <div className='Main'>
+
+{/* let's map through all slides to create an icon that moves to the next slide onclick */}
+
+        <div className='Main'>        
           {
             data.map((data, currentState)=> {
               return <span
@@ -43,7 +64,7 @@ const Slider = () => {
           }
         </div>
       </div>
-
+ 
       
     </div>
   )
